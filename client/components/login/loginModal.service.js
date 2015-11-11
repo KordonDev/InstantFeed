668bf12b3dd9103f132002d536aabcaaee86e214
Arne Maier <Arne_Maier@gmx.de>
2015-11-11 13:57:11 +0100

    Email is focused after opening login Modal.

--- client/components/login/loginModal.service.js
+++ client/components/login/loginModal.service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('instantFeedApp')
-  .factory('loginModal', function loginModal($injector) {
+  .factory('loginModal', function loginModal($injector, $timeout, $window) {
     return {
       open: open
     };
@@ -13,6 +13,15 @@ angular.module('instantFeedApp')
         controller: 'LoginController as login',
         size: 'sm'
       });
+
+      loginModal.opened.then(function() {
+        $timeout(function() {
+          var element = $window.document.getElementById('email');
+          if (element) {
+            element.focus();
+          }
+        });
+      })
     }
 
   });

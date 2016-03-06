a2f03933ddae16460bb3471e68fb5992eca5af71
Arne Maier <Arne_Maier@gmx.de>
2016-03-06 11:10:49 +0100

    Pictures with headline in lightbox.

--- client/app/message/message.directive.js
+++ client/app/message/message.directive.js
@@ -12,8 +12,8 @@ angular.module('instantFeedApp')
         var vm = this;
         vm.isPublisher = Auth.isPublisher;
 
-        vm.openModal = function(pictureUrl) {
-          Lightbox.openModal([pictureUrl], 0);
+        vm.openModal = function(message) {
+          Lightbox.openModal([{'caption': message.headline, 'url': message.picture}], 0);
         };
 
         vm.changeMessage = function(message, image) {
